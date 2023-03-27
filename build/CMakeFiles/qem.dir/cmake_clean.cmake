df226b7aeb0b6f52c79abd21e2a28fd346e400ac
Hari Hara Gowtham <gowthamharihara@gmail.com>
2023-03-27 15:47:37 +0200

    Compilation issue solved

---
+++ build/CMakeFiles/qem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qem.dir/main.cpp.o"
+  "CMakeFiles/qem.dir/main.cpp.o.d"
+  "qem"
+  "qem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
